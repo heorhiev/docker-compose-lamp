fc3f0d9b5802cf432bb459375af61619e598cd62
dev8 <dev8.sofona@gmail.com>
2023-12-14 18:25:09 +0200

    +init

---
+++ bin/baseInstaller/repo.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+echo "Running project builder"
+
+HOME_DIR=$PWD
+
+rm -rfd ./project
+
+git config --global --add safe.directory '*'
+
+git clone -b ${BASE_BRANCH} http://${GITLAB_AUTH}@${PROJECT_REPO} ./project
+
+MODULES="${DEFAULT_MODULES} $(cat ./project/common/modules.txt)"
+MODULES="${MODULES//$'\n'/ }"
+
+
+
+if [ ! -f "$PROJECT_FOLDER/README.md" ]; then
+  echo "Clone main repo (branch $BASE_BRANCH)"
+  rm -rf ${PROJECT_FOLDER} && mkdir -p ${PROJECT_FOLDER}
+  git clone -b ${BASE_BRANCH} http://${GITLAB_AUTH}@gitlab.sofona.com:1500/sofonateam/crm/base.git ${PROJECT_FOLDER}
+elif [ $UPDATE_REPOS ]; then
+  echo "Update main repo (branch $BASE_BRANCH)"
+  cd ${PROJECT_FOLDER} && git checkout -f ${BASE_BRANCH} && git pull
+fi
+
+
+echo "install modules"
+for i in ${MODULES}
+do
+  MODULE_NAME=$(echo "$i" | awk -F'/' '{print $NF}')
+
+  if [[ "$MODULE_NAME" == "keyPaymentsSystems" ]]; then
+    MODULE_NAME="keyPaymentSystems"
+  fi
+
+  if [ ! -d "$PROJECT_FOLDER/crm/common/modules/$MODULE_NAME" ]; then
+    echo "Clone module $MODULE_NAME"
+    git clone -b ${BASE_BRANCH} http://${GITLAB_AUTH}@gitlab.sofona.com:1500/sofonateam/${i}.git ${PROJECT_FOLDER}/crm/common/modules/${MODULE_NAME}
+    chmod 0755 ${PROJECT_FOLDER}/crm/common/modules/${MODULE_NAME}
+  else
+    echo "Update module $MODULE_NAME"
+    cd "$PROJECT_FOLDER/crm/common/modules/$MODULE_NAME" && git pull
+  fi
+done
+
+echo "check template"
+
+TEMPLATE_PATH="$PROJECT_FOLDER/crm/common/templates/$TEMPLATE_NAME"
+if [ -n "$TEMPLATE_NAME" ] && [ ! -d ${TEMPLATE_PATH} ]; then
+  echo "Clone template repo (branch $BASE_BRANCH)"
+  git clone -b ${BASE_BRANCH} http://${GITLAB_AUTH}@${TEMPLATE_REPO} ${TEMPLATE_PATH}
+elif [ -n "$TEMPLATE_NAME" ]; then
+  echo "Update template repo (branch $BASE_BRANCH)"
+  cd ${TEMPLATE_PATH} && git checkout -f ${BASE_BRANCH} && git pull
+fi
+
+
+echo "install config"
+cd ${HOME_DIR}
+
+if [ ! -f "$PROJECT_FOLDER/crm/common/config/main-local.php" ]; then
+  for FOLDER in frontend backend common
+  do
+    echo "cp $FOLDER config"
+
+    cp ./project/${FOLDER}/config/main-local.php ${PROJECT_FOLDER}/crm/${FOLDER}/config/main-local.php
+    cp ./project/${FOLDER}/config/params-local.php ${PROJECT_FOLDER}/crm/${FOLDER}/config/params-local.php
+
+    chmod 0755 ${PROJECT_FOLDER}/crm/${FOLDER}/config/main-local.php
+    chmod 0755 ${PROJECT_FOLDER}/crm/${FOLDER}/config/params-local.php
+  done
+
+  echo "config cp end"
+
+elif [ $UPDATE_REPOS ]; then
+  echo "config exists!"
+fi
+
+
+echo "baseInstaller end"
+
+#  chown 1000:1000 ${PROJECT_FOLDER}/crm/common/modules/${i}/.git/FETCH_HEAD ${PROJECT_FOLDER}/crm/common/modules/${i}/.git/ORIG_HEAD
+
---
+++ www/deploy.sh
@@ -0,0 +1,24 @@
+echo "127.0.0.1	crm.$DOMAIN" >> /etc/hosts.conf
+echo "127.0.0.1	my.$DOMAIN"  >> /etc/hosts.conf
+echo "127.0.0.1 $DOMAIN"     >> /etc/hosts.conf
+echo "127.0.0.1	api.$DOMAIN" >> /etc/hosts.conf
+
+a2ensite crm.${DOMAIN};
+a2ensite my.${DOMAIN};
+a2ensite ${DOMAIN};
+a2ensite api.${DOMAIN};
+
+service apache2 reload;
+
+apt-get update
+apt-get install unzip
+
+su www-data
+
+cd /var/www/html/keycrm/base/crm
+
+echo "No" | php /var/www/html/keycrm/base/crm/init --env=Development
+
+echo "y" | composer update --ignore-platform-reqs
+
+echo "y" | php /var/www/html/keycrm/base/crm/yii migrate
